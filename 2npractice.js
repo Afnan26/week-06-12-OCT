74ecb1364e130aba030eb8a0fecabc390bd15f6c
iamafnan <alsharifafnan20@gmail.com>
2019-10-08 15:34:51 +0300

    change html style by using JS

--- 2npractice.js
+++ 2npractice.js
@@ -9,3 +9,6 @@ function myFunction(){
 function newfunction(){
     var paragraph = document.write("This is a message");
 }
+
+document.getElementById("h3").style.background = "Green";
+    document.getElementById("h3").style.fontSize = "50px" ;
