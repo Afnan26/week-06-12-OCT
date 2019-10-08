c97a47eef58250740baae9e0e2d79c69cf57bacd
iamafnan <alsharifafnan20@gmail.com>
2019-10-08 14:21:08 +0300

    work on my first js functions + update on JS + HTML

--- 2npractice.js
+++ 2npractice.js
@@ -1,2 +1,11 @@
-console.log("Test JS");
 
+function myFunction(){
+    window.alert('hello');
+}
+
+
+    console.log('fghj');
+
+function newfunction(){
+    var paragraph = document.write("This is a message");
+}
